e5f4fbd836ef3e8df44bc539c5c600924c2c924e
francia5 <francia.va.or@gmail.com>
2018-06-27 17:55:05 -0500

    se borran elementos comentados de prueba

--- js/signin.js
+++ js/signin.js
@@ -10,27 +10,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
-// $("#login").click(function () {
-//     firebase.auth()
-//         .signInWithPopup(provider)
-//         .then(function (data) {
-//             var user = data.user;
-//             console.log(user);
-//             $(".inicio").hide();
-//             $(".usuario").show();
-//             $(".user-image").attr("src", user.photoURL);
-//             $("#name").text("Nombre: " + user.displayName);
-//             $("#email").text(user.email);
-
-
-//         }).catch(function (error) {
-//             console.log(error);
-
-//         });
-// });
-
-
+// Se obtiene la data del usuario al aceptar
 $('#login').click(function () {
     firebase.auth()
         .signInWithPopup(provider)
@@ -49,35 +29,3 @@ $('#login').click(function () {
 });
 
 
-
-// Mostramos o no la información del usuario de acuerdo a su status
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // Si el usuario esta logeado
-        $(".inicio").hide();
-        $(".usuario").show();
-        $(".user-image").attr("src", user.photoURL);
-        $("#name").text("Nombre: " + user.displayName);
-        $("#email").text(user.email);
-    } else {
-        // Si no esta logeado
-        $(".inicio").show();
-        $(".usuario").hide();
-    }
-});
-
-
-$("#close").click(function () {
-    firebase.auth().signOut().then(function () {
-        $(".inicio").show();
-        $(".usuario").hide();
-    })
-});
-
-
-
-firebase.auth().signOut().then(function () {
-    // Sign-out successful.
-}).catch(function (error) {
-    // An error happened.
-});
