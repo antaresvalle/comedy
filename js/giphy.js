009354be0c7e221bbd9b0bf378a3759e63500b75
DolceLemus <dulce.lemus.lara@gmail.com>
2018-06-27 21:46:41 -0500

    resolvin issues

--- js/giphy.js
+++ js/giphy.js
@@ -101,18 +101,18 @@ var templateCard = '<div class="card containerNewsfeed">'+
 
 '</div>';
 
-// FRANCIA
+// FRANCIA ma
 var provider = new firebase.auth.GoogleAuthProvider();
 
-// var config = {
-//     apiKey: "AIzaSyDFuzbP725qXoImrSmo6nov90OiPBtHnmw",
-//     authDomain: "comedy-app.firebaseapp.com",
-//     databaseURL: "https://comedy-app.firebaseio.com",
-//     projectId: "comedy-app",
-//     storageBucket: "comedy-app.appspot.com",
-//     messagingSenderId: "710197328215"
-// };
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyDFuzbP725qXoImrSmo6nov90OiPBtHnmw",
+    authDomain: "comedy-app.firebaseapp.com",
+    databaseURL: "https://comedy-app.firebaseio.com",
+    projectId: "comedy-app",
+    storageBucket: "comedy-app.appspot.com",
+    messagingSenderId: "710197328215"
+};
+firebase.initializeApp(config);
 
 
 
